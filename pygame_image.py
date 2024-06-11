10e78f3820112f8974c41acfd086e30e75a8de59
egami <c0a2302451@edu.tue.ac.jp>
2024-06-11 16:44:52 +0900

    練習8:こうかとんの移動の変更

--- pygame_image.py
+++ pygame_image.py
@@ -34,11 +34,11 @@ def main():
         key_lst = pg.key.get_pressed() # 全キーの状態を取得
         if key_lst[pg.K_UP]:
             kk_rct.move_ip((0, -1))
-        elif key_lst[pg.K_DOWN]:
+        if key_lst[pg.K_DOWN]:
             kk_rct.move_ip((0, 1))
-        elif key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1, 0))  
-        elif key_lst[pg.K_RIGHT]:
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-2, 0))  
+        if key_lst[pg.K_RIGHT]:
             kk_rct.move_ip((1, 0))              
 
         # こうかとんの座標
